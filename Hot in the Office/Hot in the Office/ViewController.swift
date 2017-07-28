1b17f3a779f0e45bce55f4848441263dd78603aa
James Addyman <james.addyman@rantmedia.com>
2017-07-28 10:09:54 +0100

    Read current temp from Firebase DB and update simple UI

--- Hot in the Office/Hot in the Office/ViewController.swift
+++ Hot in the Office/Hot in the Office/ViewController.swift
@@ -11,13 +11,32 @@ import Firebase
 
 class ViewController: UIViewController {
 
-	var dbRef: DatabaseReference!
+	@IBOutlet weak var temperatureLabel: UILabel!
+	
+	var databaseReference: DatabaseReference!
+	var temperatureHandle: DatabaseHandle?
+	
+	var currentTemperature: Double = 0.0 {
+		didSet {
+			print("temp: \(currentTemperature)")
+			temperatureLabel.text = String(format: "%.1f°C", currentTemperature)
+		}
+	}
+	
+	deinit {
+		if let temperatureHandle = temperatureHandle {
+			databaseReference.removeObserver(withHandle: temperatureHandle)
+		}
+	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		dbRef = Database.database().reference()
+		databaseReference = Database.database().reference()
+		temperatureHandle = databaseReference.observe(.value) { [unowned self] snapshot in
+			let temperatureDict = snapshot.value as? [String: AnyObject] ?? [:]
+			self.currentTemperature = (temperatureDict["current_temperature"] as? Double) ?? 0.0
+		}
 	}
-
 }
 
